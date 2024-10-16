8ba8a5825793eb19614ae67d6de13bc7dfb897b2
jiayishen21 <lucasshen21@gmail.com>
2024-10-15 20:35:29 -0400

    set up env

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/jiayishen21/resume-comp-backend/cmd/api"
+	"github.com/jiayishen21/resume-comp-backend/config"
+	"github.com/jiayishen21/resume-comp-backend/db"
 )
 
 func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(config.Envs.DBAddress)
+	log.Println(config.Envs.DBPassword)
+	initStorage(db)
+
 	server := api.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Successfully connected to DB")
 }
